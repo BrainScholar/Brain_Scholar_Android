514cdc18c24948390816ed6a617360337a879776
derekroberts <derek.roberts@kysu.edu>
2017-06-08 15:15:51 -0400

    Tweaks for graphing.

--- app/src/main/java/brainscholar/brainscholar/MainActivity.java
+++ app/src/main/java/brainscholar/brainscholar/MainActivity.java
@@ -1,47 +1,32 @@
 package brainscholar.brainscholar;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView arrayOutput;
-    EditText u0;
-    EditText v0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        arrayOutput = (TextView) findViewById(R.id.arrayOutput);
-        u0 = (EditText) findViewById(R.id.u0);
-        v0 = (EditText) findViewById(R.id.v0);
+        EditText u0 = (EditText)findViewById(R.id.u0);
+        EditText v0 = (EditText)findViewById(R.id.v0);
         Button calcButton = (Button) findViewById(R.id.calcButton);
 
-        calcButton.setOnClickListener(new View.OnClickListener() {
+        calcButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
+            public void onClick(View vw) {
+
+                EditText u0 = (EditText)findViewById(R.id.u0);
+                EditText v0 = (EditText)findViewById(R.id.v0);
+
                 double c = 0.025;
                 double gna = 0.9;
                 double gk = 1.1;
@@ -55,10 +40,10 @@ public class MainActivity extends AppCompatActivity {
                 double[] u = new double[30];
                 double[] v = new double[30];
 
+
                 u[0] = Double.parseDouble(u0.getText().toString());
 
-                v[0] = Double.parseDouble(u0.getText().toString());
-
+                v[0] = Double.parseDouble(v0.getText().toString());
 
                 for (int i = 0; i < 29; i++) {
                     /*int stinum = i/3000;
@@ -72,38 +57,13 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 }
-
-                String value = "";
-                for(int i = 0; i < 30; i++) {
-                    value += f[i];
-                }
-                    arrayOutput.setText(value);
-
+                Intent intent = new Intent(MainActivity.this, FullscreenActivity.class);
+                intent.putExtra("v", v);
+                startActivity(intent);
 
             }
         });
-
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
