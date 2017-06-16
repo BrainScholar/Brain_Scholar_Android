9ef2e76b4de4fb2a0a7ce9af1304c3a1a6af3bb8
nohtewolde <noh.tewolde@kysu.edu>
2017-06-16 14:16:26 -0400

    16/06/2017 Unlimited cycle of graph with small (size 1000) circular array

--- app/src/main/java/brainscholar/brainscholar/FullscreenActivity.java
+++ app/src/main/java/brainscholar/brainscholar/FullscreenActivity.java
@@ -55,9 +55,9 @@ public class FullscreenActivity extends AppCompatActivity {
                 double beta = getIntent().getDoubleExtra("beta", 0.6);
                 double gamma = getIntent().getDoubleExtra("gamma", 1.0);
                 double v_stim = getIntent().getDoubleExtra("v_stim", 0.9);
-                double[] f = new double[20000];
-                double[] u = new double[20000];
-                final double[] v = new double[20000];
+                double[] f = new double[10000];
+                double[] u = new double[10000];
+                final double[] v = new double[10000];
 
                 double del_t = 0.001;
                 int cl = 30;
@@ -67,29 +67,29 @@ public class FullscreenActivity extends AppCompatActivity {
                 v[0] = -1.2;
 
 
-                for (int i = 0; i < 20000; ) {
+                for (int i = 0; i < 10000; ) {
                     double floor = i / 3000;
                     double stinum = Math.floor(floor);
                     Double stimt = 3000 + 3000 * (stinum - 1);
                     Integer intstim = stimt.intValue();
                     iteration++;
 
-                    f[i%10000] = v[i%10000] * (1 - ((v[i%10000] * v[i%10000]) / 3));
-                    v[(i + 1)%10000] = 1 / c * (gna * f[i%10000] - gk * u[i%10000]) * del_t + v[i%10000];
+                    f[i%5000] = v[i%5000] * (1 - ((v[i%5000] * v[i%5000]) / 3));
+                    v[(i + 1)%5000] = 1 / c * (gna * f[i%5000] - gk * u[i%5000]) * del_t + v[i%5000];
                     if (intstim.equals(i)) {
-                        v[(i + 1)%10000] = v[(i + 1)%10000] + v_stim;
+                        v[(i + 1)%5000] = v[(i + 1)%5000] + v_stim;
                     }
-                    u[(i + 1)%10000] = (v[i%10000] + beta - gamma * u[i%10000]) * del_t + u[i%10000];
+                    u[(i + 1)%5000] = (v[i%5000] + beta - gamma * u[i%5000]) * del_t + u[i%5000];
                     runOnUiThread(new Runnable() {
 
                         @Override
                         public void run() {
-                            series.appendData(new DataPoint(iteration, v[iteration%10000]), true, 100000);
+                            series.appendData(new DataPoint(iteration, v[iteration%5000]), true, 100000);
                             //System.out.println(v[iteration]);
                         }
                     });
 
-                    i=(i+1)%10000;
+                    i=(i+1)%5000;
 
                     // sleep to slow down the add of entries
                     try {
