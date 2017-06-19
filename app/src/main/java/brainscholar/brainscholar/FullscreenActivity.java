38fb2c8867d6e165df54b87ecf065728f142157a
DerekMRoberts <derek.roberts@kysu.edu>
2017-06-19 16:04:39 -0400

    New dropdown graph speed option (slow down for less powerful devices). Also, x graph values change.

--- app/src/main/java/brainscholar/brainscholar/FullscreenActivity.java
+++ app/src/main/java/brainscholar/brainscholar/FullscreenActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
@@ -16,26 +17,38 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 public class FullscreenActivity extends AppCompatActivity {
 
     private LineGraphSeries<DataPoint> series;
-    /*public Intent intent = getIntent();
-    public double c = intent.getDoubleExtra("c", 0.025);
-    public double gna = intent.getDoubleExtra("gna", 0.9);
-    public double gk = intent.getDoubleExtra("gk", 1.1);
-    public double beta = intent.getDoubleExtra("beta", 0.6);
-    public double gamma = intent.getDoubleExtra("gamma", 1.0);
-    public double v_stim = intent.getDoubleExtra("v_stim", 0.9);*/
+    public double c;
+    public double gna;
+    public double gk;
+    public double beta;
+    public double gamma;
+    public double v_stim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
         Intent intent = getIntent();
+
         // we get graph view instance
         GraphView graph = (GraphView) findViewById(R.id.graph);
         graph.setBackgroundColor(getResources().getColor(android.R.color.white));
         // data
         series = new LineGraphSeries<DataPoint>();
         graph.addSeries(series);
-        // customize a little bit viewport
+        // customize viewport
+        graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    // show custom x values
+                    return super.formatLabel(value/10, isValueX);
+                } else {
+                    // show custom y values
+                    return super.formatLabel(value, isValueX);
+                }
+            }
+        });
         Viewport viewport = graph.getViewport();
 
 
@@ -45,7 +58,9 @@ public class FullscreenActivity extends AppCompatActivity {
         viewport.setMinX(0);
         viewport.setMaxX(10000);
         viewport.setXAxisBoundsManual(true);
+        viewport.setYAxisBoundsManual(true);
         viewport.setScrollable(true);
+        viewport.setScalable(true);
         viewport.scrollToEnd();
     }
 
@@ -60,19 +75,12 @@ public class FullscreenActivity extends AppCompatActivity {
             @Override
             public void run() {
                 // we add new entries
-                    /*double c = getIntent().getDoubleExtra("c", 0.025);
-                    double gna = getIntent().getDoubleExtra("gna", 0.9);
-                    double gk = getIntent().getDoubleExtra("gk", 1.1);
-                    double beta = getIntent().getDoubleExtra("beta", 0.6);
-                    double gamma = getIntent().getDoubleExtra("gamma", 1.0);
-                    double v_stim = getIntent().getDoubleExtra("v_stim", 0.9);*/
-
-                    /*SeekBar C = (SeekBar) findViewById(R.id.c);
-                    SeekBar GNA = (SeekBar) findViewById(R.id.gna);
-                    SeekBar GK = (SeekBar) findViewById(R.id.gk);
-                    SeekBar BETA = (SeekBar)findViewById(R.id.beta);
-                    SeekBar GAMMA = (SeekBar) findViewById(R.id.gamma);
-                    SeekBar V_STIM = (SeekBar) findViewById(R.id.v_stim);*/
+                c = getIntent().getDoubleExtra("c", 0.025);
+                gna = getIntent().getDoubleExtra("gna", 0.9);
+                gk = getIntent().getDoubleExtra("gk", 1.1);
+                beta = getIntent().getDoubleExtra("beta", 0.6);
+                gamma = getIntent().getDoubleExtra("gamma", 1.0);
+                v_stim = getIntent().getDoubleExtra("v_stim", 0.9);
 
                 Button calcButton2 = (Button) findViewById(R.id.calcButton2);
                 calcButton2.setOnClickListener(new View.OnClickListener() {
@@ -84,80 +92,20 @@ public class FullscreenActivity extends AppCompatActivity {
                         SeekBar BETA = (SeekBar) findViewById(R.id.beta);
                         SeekBar GAMMA = (SeekBar) findViewById(R.id.gamma);
                         SeekBar V_STIM = (SeekBar) findViewById(R.id.v_stim);
-                        double c = C.getProgress();
+                        c = C.getProgress();
                         c = c/1000;
-                        double gna = GNA.getProgress();
+                        gna = GNA.getProgress();
                         gna = gna/10;
-                        double gk = GK.getProgress();
+                        gk = GK.getProgress();
                         gk = gk/10;
-                        double beta = BETA.getProgress();
+                        beta = BETA.getProgress();
                         beta = beta/10;
-                        double gamma = GAMMA.getProgress();
+                        gamma = GAMMA.getProgress();
                         gamma = gamma/10;
-                        double v_stim = V_STIM.getProgress();
+                        v_stim = V_STIM.getProgress();
                         v_stim = v_stim/10;
-
-                        System.out.println(c);
-                        System.out.println(gna);
-                        System.out.println(gk);
-                        System.out.println(beta);
-                        System.out.println(gamma);
-                        System.out.println(v_stim);
-
-                        double[] f = new double[10000];
-                        double[] u = new double[10000];
-                        final double[] V = new double[10000];
-
-                        double del_t = 0.001;
-                        int cl = 30;
-                        int T = cl * 4;
-                        Double num = T / del_t;
-                        u[0] = -1.1;
-                        V[0] = -1.2;
-
-
-                        for (int i = 0; i < 10000; i = (i + 1) % 6000) {
-
-                            double floor = i / 3000;
-                            double stinum = Math.floor(floor);
-                            Double stimt = 3000 + 3000 * (stinum - 1);
-                            Integer intstim = stimt.intValue();
-                            iteration++;
-
-                            f[i % 6000] = V[i % 6000] * (1 - ((V[i % 6000] * V[i % 6000]) / 3));
-                            V[(i + 1) % 6000] = 1 / c * (gna * f[i % 6000] - gk * u[i % 6000]) * del_t + V[i % 6000];
-                            if (intstim.equals(i)) {
-                                V[(i + 1) % 6000] = V[(i + 1) % 6000] + v_stim;
-                            }
-                            u[(i + 1) % 6000] = (V[i % 6000] + beta - gamma * u[i % 6000]) * del_t + u[i % 6000];
-                            runOnUiThread(new Runnable() {
-
-                                @Override
-                                public void run() {
-                                    series.appendData(new DataPoint(iteration, V[iteration % 6000]), true, 10000);
-                                    //System.out.println(v[iteration]);
-                                }
-                            });
-
-
-                            // sleep to slow down the add of entries
-                            try {
-                                Thread.sleep(2);
-                            } catch (InterruptedException e) {
-                                // manage error ...
-                            }
-                        }
                     }
                 });
-
-                double c = getIntent().getDoubleExtra("c", 0.025);
-                double gna = getIntent().getDoubleExtra("gna", 0.9);
-                double gk = getIntent().getDoubleExtra("gk", 1.1);
-                double beta = getIntent().getDoubleExtra("beta", 0.6);
-                double gamma = getIntent().getDoubleExtra("gamma", 1.0);
-                double v_stim = getIntent().getDoubleExtra("v_stim", 0.9);
-
-
 
                 double[] f = new double[10000];
                 double[] u = new double[10000];
@@ -192,14 +140,14 @@ public class FullscreenActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             series.appendData(new DataPoint(iteration, v[iteration % 6000]), true, 10000);
-                            //System.out.println(v[iteration]);
                         }
                     });
 
-
+                    int speed = getIntent().getIntExtra("speed", 2);
+                    System.out.println(speed);
                     // sleep to slow down the add of entries
                     try {
-                        Thread.sleep(2);
+                        Thread.sleep(speed);
                     } catch (InterruptedException e) {
                         // manage error ...
                     }
--- app/src/main/java/brainscholar/brainscholar/MainActivity.java
+++ app/src/main/java/brainscholar/brainscholar/MainActivity.java
@@ -4,17 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 
 
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
-public class MainActivity extends AppCompatActivity {
+    private int speed;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.speed_arrays, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(MainActivity.this);
 
         Button calcButton = (Button) findViewById(R.id.calcButton);
         calcButton.setOnClickListener(new View.OnClickListener(){
@@ -46,9 +57,12 @@ public class MainActivity extends AppCompatActivity {
 
                 double v_stim = V_STIM.getProgress();
                 v_stim = v_stim/10;
+                
 
+                System.out.println(speed);
 
                 Intent intent = new Intent(MainActivity.this, FullscreenActivity.class);
+                intent.putExtra("speed", speed);
                 intent.putExtra("c", c);
                 intent.putExtra("gna", gna);
                 intent.putExtra("gk", gk);
@@ -60,6 +74,34 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                speed = 1;
+                break;
+            case 1:
+                speed = 2;
+                break;
+            case 2:
+                speed = 3;
+                break;
+            case 3:
+                speed = 4;
+                break;
+            case 4:
+                speed = 5;
+                break;
+
+        }
+        System.out.println();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        speed = 2;
+
     }
 
 
