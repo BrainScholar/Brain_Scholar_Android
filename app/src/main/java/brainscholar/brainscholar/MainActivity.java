62b2fa735aee0071dc4a004947697109d8732e32
nohtewolde <noh.tewolde@kysu.edu>
2017-06-21 13:05:50 -0400

    21/06/2017 Updated SeekBar tumb value with progress

--- app/src/main/java/brainscholar/brainscholar/MainActivity.java
+++ app/src/main/java/brainscholar/brainscholar/MainActivity.java
@@ -1,27 +1,163 @@
 package brainscholar.brainscholar;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
-
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SeekBar C = (SeekBar) findViewById(R.id.c);
+        SeekBar GNA = (SeekBar) findViewById(R.id.gna);
+        SeekBar GK = (SeekBar) findViewById(R.id.gk);
+        SeekBar BETA = (SeekBar)findViewById(R.id.beta);
+        SeekBar GAMMA = (SeekBar) findViewById(R.id.gamma);
+        SeekBar V_STIM = (SeekBar) findViewById(R.id.v_stim);
+
+        GNA.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    TextView text_SeekBar = (TextView) findViewById(R.id.textGNAprogress);
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
+                        text_SeekBar.setText("" + progress/10.0);
+                        text_SeekBar.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                        text_SeekBar.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        text_SeekBar.setVisibility(View.GONE);
+                    }
+                }
+        );
+
+        GK.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    TextView text_SeekBar = (TextView) findViewById(R.id.textGKprogress);
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
+                        text_SeekBar.setText("" + progress/10.0);
+                        text_SeekBar.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                        text_SeekBar.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        text_SeekBar.setVisibility(View.GONE);
+                    }
+                }
+        );
+
+        BETA.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    TextView text_SeekBar = (TextView) findViewById(R.id.textBETAprogress);
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
+                        text_SeekBar.setText("" + progress/10.0);
+                        text_SeekBar.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                        text_SeekBar.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        text_SeekBar.setVisibility(View.GONE);
+                    }
+                }
+        );
+
+        GAMMA.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    TextView text_SeekBar = (TextView) findViewById(R.id.textGAMMAprogess);
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
+                        text_SeekBar.setText("" + progress/10.0);
+                        text_SeekBar.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                        text_SeekBar.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        text_SeekBar.setVisibility(View.GONE);
+                    }
+                }
+        );
+
+        V_STIM.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    TextView text_SeekBar = (TextView) findViewById(R.id.textV_STIMprogress);
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
+                        text_SeekBar.setText("" + progress/10.0);
+                        text_SeekBar.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                        text_SeekBar.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        text_SeekBar.setVisibility(View.GONE);
+                    }
+                }
+        );
+
+        C.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    TextView text_SeekBar = (TextView) findViewById(R.id.textCprogress);
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
+                        text_SeekBar.setText("" + progress/1000.0);
+                        text_SeekBar.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                        text_SeekBar.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        text_SeekBar.setVisibility(View.GONE);
+                    }
+                }
+        );
 
         Button calcButton = (Button) findViewById(R.id.calcButton);
         calcButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View vw) {
-
-
                 SeekBar C = (SeekBar) findViewById(R.id.c);
                 SeekBar GNA = (SeekBar) findViewById(R.id.gna);
                 SeekBar GK = (SeekBar) findViewById(R.id.gk);
